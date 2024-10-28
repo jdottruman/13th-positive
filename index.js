42a083bbfa55584a698c84ca3fd7b6630692e286
jay <jadyntruman2@gmail.com>
2024-10-28 14:03:18 -0500

    members visible

--- index.js
+++ index.js
@@ -61,112 +61,8 @@ const resources = [
     thirdName: "Re-Entry Employment Service Program",
   },
 ];
-const members = [
-  {
-    photo: "Photos/regHeadshot.png",
-    name: "Reginald <br> Neal",
-    role: "President",
-    message:
-      "Reginald Neal has been a lifelong resident of Maywood, proudly calling it home for 52 years. As a former business owner in the village, he holds a deep connection to the community. For the past 14 years, he has been employed by The Law Offices of Blitt and Gaines, P.C., where he currently serves as a supervisor—a role he has held for the past six years. <br> In his personal life, Reginald has enjoyed 24 years of marriage with his wonderful wife, Kasonjia Neal. He is a proud father of six children—four daughters (one of whom is sadly no longer with us) and two sons—and a devoted grandfather to eight cherished grandchildren: five lovely granddaughters and three handsome grandsons.",
-  },
-  {
-    photo: "Photos/anthony headshot.png",
-    name: "Anthony <br> Lindsey",
-    role: "Board Member",
-    message:
-      "Anthony is a dedicated board member of the 13th Positive Foundation, committed to empowering communities through education, mentorship, and positive change. Alongside his board responsibilities, Anthony is responsible for marketing and managing the social media presence of 13th Positive, ensuring the foundation’s mission reaches a wider audience and continues to inspire and uplift those it serves.Anthony is also a passionate youth football and basketball coach, mentoring young athletes to excel both on and off the field. With extensive leadership experience and a deep commitment to fostering growth and opportunity, Anthony plays a key role in shaping the foundation’s strategic vision and supporting its initiatives that create lasting, positive impacts in the community. By combining his love for sports, mentorship, and community building, Anthony’s contributions help ensure 13th Positive continues to grow, while empowering the next generation of leaders and athletes.",
-  },
-  {
-    photo: "Photos/image0.png",
-    name: "Antwan <br> Coffie ",
-    role: "Board Member",
-    message:
-      "Antwan serves as a board member of the 13th Positive Foundation, an organization focused on fostering community development through education, mentorship, and impactful change. A lifelong resident of Maywood for 47 years, he found the love of his life in the same community. His wife, a beloved educational leader, left a lasting legacy even after her passing. <br> Antwan raised two daughters, Kennedy and Brittanie—both master’s degree holders with strong community connections—and now mentors his son, Antwan Jr., who hopes to attend Proviso Math and Science Academy (PMSA). Deeply understanding the importance of family unity, Antwan remains committed to both his loved ones and the broader community. <br> In addition to his work with 13th Positive, he has coached youth baseball and youth boxing, shaping future generations through sports and mentorship. Dedicated to honoring the Coffie name, Antwan upholds his family’s legacy while contributing to a united, resilient future for Maywood.",
-  },
-  {
-    photo: "Photos/chuck.png",
-    name: "Charles M.<br> Stump ",
-    role: "Vice President",
-    message:
-      "Charles is a barber, entrepreneur, and investor with roots going back four generations in Maywood. He has spent the majority of his life in the Proviso Township, supporting local initiatives to rebuild, restore, and reinvigorate the area and the children within it. With over 25 years as a neighborhood barber, Charles has used his role to mentor from behind the chair and collaborate with grassroots organizations. His partnerships include participating in community clean-ups, fundraising, and supporting various efforts for community partners, reflecting his deep commitment to service and community growth.",
-  },
-
-  {
-    photo: "Photos/clarence.png",
-    name: "Clarence <br> Frison ",
-    role: "Board Member",
-    message:
-      "Clarence Frison is a proud board member of the 13th Positive Foundation. He has been a devoted pastor for over 20 years, passionately serving the Lord Jesus Christ. Clarence is a loving husband to his wife, Kenya, with whom he has shared 8 wonderful years of marriage. Together, they are blessed with 10 children—7 boys and 3 girls—and 16 grandchildren. Originally from Maywood, Illinois, Clarence now calls Mississippi home, where he has dedicated his life to serving others and uplifting his community through faith and leadership.",
-  },
-  {
-    photo: "Photos/clay2.png",
-    name: "Clayton <br> Howard ",
-    role: "Board Member",
-    message:
-      "Clayton Howard is a passionate entrepreneur with a lifelong commitment to business excellence. Throughout his career, he has built a track record of success by embracing innovation and maintaining a strong work ethic. Happily married for 25 years, Clayton is a proud father of three and a devoted grandfather. <br> In addition to his business ventures, Clayton is dedicated to giving back to the community. He has volunteered in juvenile detention programs, mentoring young individuals and striving to make a positive impact. His life reflects a thoughtful balance between family, business, and service, embodying his roles as a loving family man, accomplished entrepreneur, and committed mentor.",
-  },
-  {
-    photo: "Photos/#",
-    name: "Craig <br> Flowers",
-    role: "Board Member",
-    message:
-      "Craig Flowers is the Ambassador of 13th Positive, a foundation dedicated to bringing positivity and change to the community. As the jokester of the group, Craig has an infectious sense of humor that keeps everyone laughing and uplifted, no matter the situation. He is a strong, loving, and kindhearted individual with an unwavering passion for people and his community, always ready to lend a helping hand or a comforting word. In his professional life, Craig is a God-gifted psalmist, known for his powerful voice and soul-stirring performances at funerals and special occasions. His voice has been a source of comfort for countless families, and his reputation as a talented and compassionate artist extends far beyond his local community. Craig is also a devoted family man, having been married to his wife, Yolanda Flowers, for over 24 years. Together, they have built a beautiful family, blessed with three daughters and five amazing grandchildren—four boys and one girl—whom he loves dearly. His commitment to his family is as strong as his commitment to his community, making him a beloved figure both at home and beyond.",
-  },
-  {
-    photo: "Photos/james.png",
-    name: "James<br> Truman",
-    role: "Secretary",
-    message:
-      "James Truman is an active board member and Secretary of the 13th Positive Foundation, a community-driven organization dedicated to supporting the Proviso Township through food drives, toy drives, youth scholarships, and acts of kindness. <br> A proud Chicagoan, James is a devoted fan of the Bulls, Cubs, and Bears. He lives, works, and thrives in Proviso Township, demonstrating his deep connection to the community. A committed family man, he balances his roles as a husband, father, and 'pop-pop' to two grandsons while remaining devoted to his faith, family, friends, and service.",
-  },
-  {
-    photo: "Photos/lorenzo.png",
-    name: "Lorenzo <br> Williams  ",
-    role: "Board Member",
-    message:
-      " Lorenzo Williams served as a former Secretary and currently as a board member. With over 20 years of experience, Lorenzo has been dedicated to supporting the communities and neighborhoods of Proviso Township. Lorenzo is a successful entrepreneur and the owner of ZinZo 1 Luxury Black Car Service, which he has operated for the past 8 years. In his personal life, Lorenzo is a devoted family man and grandfather, known for his adventurous spirit. He is a passionate Chicago sports fan and an avid golfer. Lorenzo takes great pride in giving back to the community where he grew up, with a strong focus on paving the way for youth development and their future success.",
-  },
-  {
-    photo: "/Photos/#",
-    name: "Martese<br> Owens",
-    role: "Board Member",
-    message:
-      "Martese Owens Board Member, 13th Positive Foundation Martese Owens, 48, has been self-employed for the past 20 years as the owner of Owens Construction and Services, specializing in all aspects of real estate and property management. A former resident of Bellwood, Broadview, and Maywood, Martese is a proud alumnus of Proviso West High School, class of 1994. He has been married to LaTanya Owens for 14 years and is a dedicated father of six children and grandfather to three. Martese has a deep love for children and is passionate about ensuring they have better opportunities for success and a brighter future. His life is built on the foundation of kindness, with a commitment to uplifting his community through his work and philanthropic efforts. He strives to create a lasting, positive impact on the Proviso community and surrounding areas, always focusing on providing the next generation with the tools they need for a better life.",
-  },
-  ,
-];
 
 let resourcesHtml = ``;
-let membersHtml = ``;
-members.forEach((member, index) => {
-  membersHtml += `
-        <div class="memberCard section__paragraph">
-          <div class="cardImg">
-            <img src="${member.photo}" alt="" />
-          </div>
-          <div class="cardText">
-            <p class="name">${member.name}</p>
-            <p class="title">${member.role}</p>
-            <button class="cardModalBtn button-6" data-index="${index}">About</button>
-            <!-- HTML !-->
-
-          </div>
-
-        </div>
-        
-        <div id="myModal-${index}" class="modal">
-                    <span class="close-btn" data-index="${index}">&times;</span>
-
-          <div class="modal-content">          
-            <img src="${member.photo}" alt="" />
-            <p class="name">${member.name}</p>
-            <p class="title">${member.role}</p>
-            <p class="memo">${member.message}</p>
-      
-          </div>
-        </div>
-  `;
-});
 
 resources.forEach((resource) => {
   resourcesHtml += `
@@ -215,8 +111,7 @@ resources.forEach((resource) => {
 // document.querySelector(".events-container").innerHTML = eventsHtml;
 
 // document.querySelector(".resources-container").innerHTML = resourcesHtml;
-document.querySelector(".cardContainer").innerHTML = membersHtml;
-
+//
 document.querySelectorAll(".cardModalBtn").forEach((btn) => {
   btn.addEventListener("click", (e) => {
     const index = e.target.getAttribute("data-index");
